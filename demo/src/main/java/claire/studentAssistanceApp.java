0a415951adba4103f222a2e816ec590433088ba5
Sockura <snguyenka@gmail.com>
2022-01-25 22:22:29 -0800

    Revert "Home screen implementation"
    This reverts commit f791aa1f110f253a236e45b62cd21ceed10f170d.

--- demo/src/main/java/claire/studentAssistanceApp.java
+++ demo/src/main/java/claire/studentAssistanceApp.java
@@ -1,6 +1,12 @@
+package claire;
+//import javax.swing.*;
+
 public class studentAssistanceApp{
     public static void main(String args[]){
         Window window = new Window();
+        //LockScreen lock = new LockScreen();
+        CalculatorScreen calculator = new CalculatorScreen();
+        
     }
 }
  
